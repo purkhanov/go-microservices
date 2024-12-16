ee6e26185c812235e9384120d6dca07bff49e2f6
Nurlan Purkhanov <ppurkhanov@gmail.com>
2024-12-16 19:41:30 +0500

    created routes and models

--- auth-service/cmd/api/main.go
+++ auth-service/cmd/api/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "8000"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Starting authentication service")
+
+	// TODO connect to db
+
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
+}
