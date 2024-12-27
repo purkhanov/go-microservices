601f3c13c4fd45c4484ae8699853de68fd2b600c
Nurlan Purkhanov <ppurkhanov@gmail.com>
2024-12-27 11:21:07 +0500

    added consumer and listener to listener service

--- listener-service/main.go
+++ listener-service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"listener/event"
 	"log"
 	"math"
 	"os"
@@ -21,10 +22,19 @@ func main() {
 	log.Println("Connected to RabbitMQ!")
 
 	// start listening for messages
+	log.Println("Listening for and consuming RabbitMQ messages...")
 
 	// create consumser
+	consumer, err := event.NewConsumer(rabbitConn)
+	if err != nil {
+		panic(err)
+	}
 
 	// watch the queue and consume events
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func connect() (*amqp.Connection, error) {
@@ -34,7 +44,7 @@ func connect() (*amqp.Connection, error) {
 
 	// don't continue until rabbitmq is ready
 	for {
-		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
 		if err != nil {
 			fmt.Println("RabbitMQ not yet ready...")
 			counts++
